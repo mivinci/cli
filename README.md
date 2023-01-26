cd9fb8736e326f46ee9d7b29af51e012b201fccf
mivinci <mivinci@qq.com>
2023-01-27 01:32:01 +0800

    added an example

--- README.md
+++ README.md
@@ -2,6 +2,20 @@
 
 A package for building command line apps. It looks like [spf13/cobra](https://github.com/spf13/cobra) but has [urfave/cli](https://github.com/urfave/cli)-like APIs and a builtin argument parser also.
 
+## Features
+
+- Nested commands
+- Positional arguments
+- Type-safe flags
+
 ## Usage
 
-writing...
+```bash
+go get -u github.com/mivinci/cli
+```
+
+See the [example](./example) for details.
+
+## License
+
+This package is Apache 2.0 licensed.
---
+++ example/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/mivinci/cli"
+)
+
+func main() {
+	root := cli.Command{
+		Name: "example",
+		Usage: `example description example description example description.
+example description example description example description
+example description example`,
+		Flags: []*cli.Flag{
+			{
+				Name:  "address",
+				Usage: "specify an IP address",
+				Value: cli.IP("127.0.0.1"),
+			},
+			{
+				Name:  "hash",
+				Usage: "specify a hash number",
+				Value: cli.Int(53748191),
+			},
+			{
+				Name:  "verbose",
+				Usage: "show tails when running",
+				Value: cli.Bool(false),
+			},
+		},
+		Run: func(ctx *cli.Context) error {
+			fmt.Println(ctx.IP("address"))
+			return nil
+		},
+	}
+	upload := cli.Command{
+		Name:  "upload",
+		Usage: "upload a file",
+		Run: func(ctx *cli.Context) error {
+			fmt.Println("a1")
+			fmt.Println(ctx.Args())
+			fmt.Println(ctx.Bool("foo"))
+			return nil
+		},
+	}
+	download := cli.Command{
+		Name:  "download",
+		Usage: "download a file",
+		Flags: []*cli.Flag{
+			{
+				Name:  "thread",
+				Short: 't',
+				Usage: "specify the number of threads",
+				Value: cli.Int(2),
+			},
+		},
+		Run: func(ctx *cli.Context) error {
+			fmt.Println(ctx.Args())
+			fmt.Println(ctx.Bool("foo"))
+			return nil
+		},
+	}
+
+	root.Add(&upload, &download)
+
+	if err := root.Exec(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
